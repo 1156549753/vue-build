dabc919fde79088a5e6049e5982ec9cf815ba35d
cat <1156549753@qq.com>
2019-07-30 16:21:40 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import './index.css'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+new Vue({
+    router,
+    render:h=>h(App)
+}).$mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div>
+            dsdsdfs
+        </div>
+    </div>
+</template>
