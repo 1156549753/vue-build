3494ab9a545b1113bc76000d0219cb6d756f43ed
cat <1156549753@qq.com>
2019-08-02 16:33:01 +0800

    fix

--- src/router.js
+++ src/router.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
-
-export default new Router({
-    routes: [
+import VueRouter from 'vue-router'; //引入插件
+Vue.use(VueRouter); //安装插件
+ 
+// 创建 router 实例，然后传 `routes` 配置
+export default new VueRouter({
+    mode: 'history',// 默认为hash模式
+    routes:[
         {
-            path: '/home',
-            name: 'home',
-            component: () => import ('./views/index.vue')
+            name:'default', 
+            path:'',
+            redirect:{name:'foo'}
+        },
+        {
+            name:'foo',
+            path:'/foo',
+            component:() => import('./view/foo.vue')
+        },
+        {
+            name:'bar',
+            path:'/bar',
+            component:() => import('./view/bar.vue')
         }
     ]
 })
